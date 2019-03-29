9ac9d447f4993e7dba56e6c36c80b8e94835ed8b
Juzcategui <gotdakey@gmail.com>
2019-03-29 11:26:18 -0400

    first commit

---
+++ routes/api-routes.js
@@ -0,0 +1,13 @@
+let employeesList = require("../data/employees.js");
+
+module.exports = function(app) {
+  app.get("/api/employeesList", function(req, res) {
+    res.json(employeesList);
+  });
+
+  app.post("/api/employeesList", function(req, res) {
+    res.json(employeesList);
+
+    res.end();
+  });
+};
